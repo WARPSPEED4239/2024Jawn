e4d455af5754072dd7bbe25be14d757d88634685
Team4239 <warpspeed@willmar.org>
2024-01-09 15:38:12 -0600

    motors id'ed

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,7 +5,20 @@ public class Constants {
         XBOX_CONTROLLER = 0,
         JOYSTICK = 1,
 
-        PIGEON_IMU = 9,
+        DRIVEMOTOR_1 = 101,
+        SWERVEMOTOR_1 = 102,
+        CAMCODER_1 = 103,
+        DRIVEMOTOR_2 = 104,
+        SWERVEMOTOR_2 = 105,
+        CAMCODER_2 = 106,
+        DRIVEMOTOR_3 = 107,
+        SWERVEMOTOR_3 = 108,
+        CAMCODER_3 = 109,
+        DRIVEMOTOR_4 = 110,
+        SWERVEMOTOR_4 = 111,
+        CAMCODER_4 = 112,
+
+        PIGEON_IMU = 200,
 
         FILLER = 5318008;
 }
